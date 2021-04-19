7f8147ecb4b046e2a40082c30f16d9b979eeec9c
Jennifer Toops <jennifertoops@gmail.com>
2021-04-19 17:56:37 -0400

    commit match() regex examples

---
+++ 04-19-2021regex.js
@@ -0,0 +1,27 @@
+//MATCH
+
+let quote = "You miss 100% of the shots you don't take.".toLowerCase()
+
+const regex1 = /you/
+
+const regex2 = /shots/
+
+quote.match(regex1)
+//["you", index: 27, input: "You miss 100% of the shots you don't take.", groups: undefined]
+
+quote.match(regex2)
+//["shots", index: 21, input: "You miss 100% of the shots you don't take.", groups: undefined]
+
+quote.match(regex1/g)
+//(2) ["you", "you"]
+
+let youQuote = quote.match(regex4).length
+youQuote;
+//2
+
+let newQuote = "Why are there so many songs about rainbows?"
+const regex3 = /rainbows/gi
+
+newQuote.match(regex3)
+//["rainbows"]
+
