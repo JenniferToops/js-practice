ea1319551355559e176922aed8f8128ef1e233f8
Jennifer Toops <jennifertoops@gmail.com>
2021-04-26 18:39:38 -0400

    committing includes method

---
+++ 04-25-21methods-what-they-return.js
@@ -0,0 +1,11 @@
+/* ShlVsv: Definition and Usage The pop() method removes the last element of an array, and returns that element.
+ShlVsv: It returns an element not an array so you can't call shift on that
+ShlVsv: when you stack it's like calling a method on the return value
+ShlVsv: When you call split() it return an array so you can use array method on that array again
+ShlVsv: Definition and Usage The split() method is used to split a string into an array of substrings, and returns the new array.
+ShlVsv: See this is the definition of spilt()
+ShlVsv: It returns an array
+ShlVsv: so you can call pop() on that
+ShlVsv: like split(" ").pop()
+ShlVsv: but you can't do pop().split(" ")
+*/
---
+++ 04-26-2021methods-include.js
@@ -0,0 +1,7 @@
+let arr = [1, 2, 3, 4, 5, "hi"]
+arr.includes("hi")
+//true
+arr. includes(3, 2)
+//true
+arr.includes(3, 3)
+//false
